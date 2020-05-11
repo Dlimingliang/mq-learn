23898e9a6bb406244198037b578830153b7d6aef
李明亮 <limingliang@izaodao.org>
2020-05-11 10:37:50 +0800

    多消费方策略

--- rabbitmq-sender/src/main/java/com/limingliang/projects/rabbitmqsender/helloworld/HelloWorldSender.java
+++ rabbitmq-sender/src/main/java/com/limingliang/projects/rabbitmqsender/helloworld/HelloWorldSender.java
@@ -6,6 +6,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Auther: limingliang
  * @Description:
@@ -22,10 +24,25 @@ public class HelloWorldSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @Scheduled(fixedDelay = 10000, initialDelay = 500)
+    AtomicInteger dots = new AtomicInteger(0);
+
+    AtomicInteger count = new AtomicInteger(0);
+
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
     public void send() {
 
-        String message = "Hello World!";
+        StringBuilder builder = new StringBuilder("Hello world");
+        if (dots.incrementAndGet() == 4) {
+            dots.set(1);
+        }
+
+        for (int i = 0; i < dots.get(); i++) {
+            builder.append(".");
+        }
+        builder.append(count.incrementAndGet());
+        String message = builder.toString();
+
         this.rabbitTemplate.convertAndSend(queue.getName(), message);
         log.info("发送消息,内容为 " + message);
     }
