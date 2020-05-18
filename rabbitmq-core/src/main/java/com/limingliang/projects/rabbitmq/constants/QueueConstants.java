37c17d33ec81d9f4c55308247ca060aa7710d6ef
李明亮 <limingliang@izaodao.org>
2020-05-18 15:28:22 +0800

    消息的发送确认开启

---
+++ rabbitmq-core/src/main/java/com/limingliang/projects/rabbitmq/constants/QueueConstants.java
@@ -0,0 +1,12 @@
+package com.limingliang.projects.rabbitmq.constants;
+
+/**
+ * @Auther: limingliang
+ * @Description:
+ * @Date: 2020/5/18 13:59
+ * Copyright (c) 2017, zaodao All Rights Reserved.
+ */
+public class QueueConstants {
+
+    public static final String orderCreateQueue = "order_create_queue";
+}
