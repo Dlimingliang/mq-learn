95ecb0bbe5b15d6cf7acac6234a9fd2ab130fdae
李明亮 <limingliang@izaodao.org>
2020-05-19 15:02:10 +0800

    消费端加入

--- rabbitmq-sender/src/main/java/com/limingliang/projects/rabbitmq/payOrder/config/RabbitmqConfig.java
+++ rabbitmq-sender/src/main/java/com/limingliang/projects/rabbitmq/payOrder/config/RabbitmqConfig.java
@@ -7,7 +7,6 @@ import com.limingliang.projects.rabbitmq.payOrder.service.MsgLogService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
