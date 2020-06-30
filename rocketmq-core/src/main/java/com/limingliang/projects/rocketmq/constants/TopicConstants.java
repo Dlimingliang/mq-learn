e008f03ae440cc182fb7388d6ce25ea3676ce588
李明亮 <limingliang@izaodao.org>
2020-06-30 15:07:15 +0800

    rocketmq starter加入

---
+++ rocketmq-core/src/main/java/com/limingliang/projects/rocketmq/constants/TopicConstants.java
@@ -0,0 +1,12 @@
+package com.limingliang.projects.rocketmq.constants;
+
+/**
+ * @Auther: limingliang
+ * @Description:
+ * @Date: 2020/6/30 15:04
+ * Copyright (c) 2017, zaodao All Rights Reserved.
+ */
+public class TopicConstants {
+
+    public static final String OrderTopic = "order_topic";
+}
