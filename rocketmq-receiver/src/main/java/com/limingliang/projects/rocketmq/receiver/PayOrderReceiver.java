67bc53c3a05f8ba6273a5306c541ada3efc1e05d
李明亮 <limingliang@izaodao.org>
2020-06-30 16:18:52 +0800

    消费群组加入

---
+++ rocketmq-receiver/src/main/java/com/limingliang/projects/rocketmq/receiver/PayOrderReceiver.java
@@ -0,0 +1,24 @@
+package com.limingliang.projects.rocketmq.receiver;
+
+import com.limingliang.projects.rocketmq.domain.PayOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+
+/**
+ * @Auther: limingliang
+ * @Description:
+ * @Date: 2020/6/30 16:12
+ * Copyright (c) 2017, zaodao All Rights Reserved.
+ */
+
+@Slf4j
+@RocketMQMessageListener(topic = "order_topic", consumerGroup = "orderConsumerGroup")
+public class PayOrderReceiver implements RocketMQListener<PayOrder> {
+
+    @Override
+    public void onMessage(PayOrder payOrder) {
+
+        log.info("接收到消息: {}" + payOrder);
+    }
+}
